aaa89024973898d84ad4cb11bcb2d8d39aec3665
Rody Vilchez <102562850+R0SEWT@users.noreply.github.com>
2026-09-05 15:10:49 -0500

    go es malvado

---
+++ labs/go/week01/tutorial/code02.go
@@ -0,0 +1,14 @@
+import "fmt"
+
+var nombre string
+
+func main() {
+	nombre = "Carlos"
+
+	apellido := "Gonzales"
+
+	edad := 30
+
+	fmt.Println("Hola", nombre, apellido, "tienes", edad, "años")
+
+}
